09e031c0028067e651896255f49774aed611ad39
VladBardin <vladyslav.bardin@gmail.com>
2023-04-25 13:42:57 +0300

    First Commit

---
+++ src/main/java/database/DatabaseInitService.java
@@ -0,0 +1,13 @@
+package database;
+
+import org.flywaydb.core.Flyway;
+
+public class DatabaseInitService {
+    public void initDb() {
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource("jdbc:h2:./test", null, null)
+                .load();
+        flyway.migrate();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import database.ClientService;
+import database.Database;
+import database.DatabaseInitService;
+import table.Client;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        new DatabaseInitService().initDb();
+        Connection date = new Database().getConnection();
+        System.out.println("client = " + new ClientService(date).listAll().toString());
+        long id = 5;
+        Client client0 = new Client();
+        client0.setId(id);
+        client0.setName(new ClientService(date).getById(id));
+        System.out.println("client = " + client0);
+        System.out.println("Last id is " + new ClientService(date).create(86, "IntIt corp."));
+        new ClientService(date).setName(5, "J.E.I.");
+        new ClientService(date).deleteById(1);
+        System.out.println("client = " + new ClientService(date).listAll().toString());
+    }
+}
